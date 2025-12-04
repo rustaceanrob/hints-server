c51b4c0e5c8608abd5ac214a696134890a23b47e
rustaceanrob <rob.netzke@gmail.com>
2025-12-04 15:20:22 +0000

    Add server scaffolding

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Quickstart for new Rust projects
+# SwiftSync hintsfile server
 
-Useful `just` commands:
-
-`just run`: run the binary in release mode
-
-`just test`: run the unit tests, doc tests (will fail if there is no library)
-
-`just delete`: delete local git branches that have been merged to master
-
-
+Serves hints of the UTXO set over the web.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    path::PathBuf,
+    str::FromStr,
+};
+
+use axum::{
+    Router,
+    body::Body,
+    http::{StatusCode, header},
+    response::{AppendHeaders, IntoResponse},
+    routing::get,
+};
+use tokio::fs::File;
+use tokio_util::io::ReaderStream;
+
+const IP_BIND_LOCAL: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
+const PORT_BIND_LOCAL: u16 = 3000;
+const LOCAL_HOST: SocketAddr = SocketAddr::new(IP_BIND_LOCAL, PORT_BIND_LOCAL);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Chain {
+    Signet,
+    Bitcoin,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(async || "SwiftSync hints assistant."))
+        .route("/hints/bitcoin", get(handle_bitcoin_hints))
+        .route("/hints/signet", get(handle_signet_hints));
+    let listener = tokio::net::TcpListener::bind(&LOCAL_HOST).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_bitcoin_hints() -> impl IntoResponse {
+    stream_hints_file(Chain::Bitcoin).await
+}
+
+async fn handle_signet_hints() -> impl IntoResponse {
+    stream_hints_file(Chain::Signet).await
+}
+
+async fn stream_hints_file(chain: Chain) -> impl IntoResponse {
+    let bitcoin_dir = std::env::var("HINTS_DIR").unwrap();
+    let bitcoin_dir_path = PathBuf::from_str(&bitcoin_dir).unwrap();
+    let hintsfile_path = match chain {
+        Chain::Signet => bitcoin_dir_path.join("signet.hints"),
+        Chain::Bitcoin => bitcoin_dir_path.join("bitcoin.hints"),
+    };
+    let file = match File::open(hintsfile_path).await {
+        Ok(file) => file,
+        Err(err) => return Err((StatusCode::NOT_FOUND, format!("File not found: {err}"))),
+    };
+    let byte_stream = ReaderStream::new(file);
+    let body = Body::from_stream(byte_stream);
+    let headers = AppendHeaders([
+        (header::CONTENT_TYPE, "binary/hints"),
+        (header::CONTENT_DISPOSITION, "file"),
+    ]);
+    Ok((headers, body))
 }
